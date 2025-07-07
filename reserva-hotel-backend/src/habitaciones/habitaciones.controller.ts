6a33e7facb4b1f13fccc58d524c365702df3c8f5
Ricardo Adalberto Romero Medina <42826880+Adal612Git@users.noreply.github.com>
2025-07-06 21:07:45 -0600

    Add habitaciones module with basic CRUD

---
+++ reserva-hotel-backend/src/habitaciones/habitaciones.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateHabitacionDto } from './dto/create-habitacion.dto';
+import { Habitacion } from './habitacion.entity';
+import { HabitacionesService } from './habitaciones.service';
+
+@Controller('habitaciones')
+export class HabitacionesController {
+  constructor(private readonly habitacionesService: HabitacionesService) {}
+
+  @Get()
+  findAll(): Habitacion[] {
+    return this.habitacionesService.findAll();
+  }
+
+  @Post()
+  create(@Body() dto: CreateHabitacionDto): Habitacion {
+    return this.habitacionesService.create(dto);
+  }
+}
